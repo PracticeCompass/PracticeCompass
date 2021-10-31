0947c5e57969f413b306ef604daaf5b12114b299
ahebala <amrhebala@gmail.com>
2021-10-31 23:48:05 +0200

    charge model

--- PracticeCompass.Core/Models/Charge.cs
+++ PracticeCompass.Core/Models/Charge.cs
@@ -1,10 +1,56 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace PracticeCompass.Core.Models
 {
     public class Charge
     {
+        public string prrowid { get; set; }
+        public int? ChargeSID { get; set; }
+        public int? PracticeID { get; set; }
+        public int? StaffID { get; set; }
+        public int? PatientID { get; set; }
+        public int? AccountSID { get; set; }
+        public int? FinanceCenterID { get; set; }
+        public int? JournalSID { get; set; }
+        public DateTime? PostDate { get; set; }
+        public string DepartmentCode { get; set; }
+        public string ChargeType { get; set; }
+        public string AdminChargeCode { get; set; }
+        public string DefaultFeeOverrode { get; set; }
+        public decimal? Amount { get; set; }
+        public decimal? ApprovedAmount { get; set; }
+        public decimal? CopayAmount { get; set; }
+        public decimal? VisitCopay { get; set; }
+        public decimal? DeductibleApplied { get; set; }
+        public decimal? TaxAmount { get; set; }
+        public string CurrentStatus { get; set; }
+        public DateTime? DateSatisfied { get; set; }
+        public string AssignBenefits { get; set; }
+        public string StmtCommentCode { get; set; }
+        public DateTime? GuarantorRespDate { get; set; }
+        public string ForcePaperClaim { get; set; }
+        public string PatientBilled { get; set; }
+        public string PatientStatement { get; set; }
+        public string RecordStatus { get; set; }
+        public string TimeStamp { get; set; }
+        public int? LastUser { get; set; }
+        public string CreateStamp { get; set; }
+        public int? CreateUser { get; set; }
+        public decimal? GuarantorReceipts { get; set; }
+        public decimal? InsuranceReceipts { get; set; }
+        public decimal? Adjustments { get; set; }
+        public string Voucher { get; set; }
+        public string RespPartyType { get; set; }
+        public int? RespPartyID { get; set; }
+        public string PolicyNumber { get; set; }
+        public int? RespCoverageOrder { get; set; }
+        public string VisitCopayClass { get; set; }
+        public int? ChargeSourceModuleID { get; set; }
+        public string SevTwoRuleBillingHold { get; set; }
+        public DateTime? SevTwoRuleBillingHoldDate { get; set; }
+        public int? SevTwoRuleOffHoldUserID { get; set; }
+        public string Pro2SrcPDB { get; set; }
+        public DateTime? pro2created { get; set; }
+        public DateTime? pro2modified { get; set; }
     }
 }
