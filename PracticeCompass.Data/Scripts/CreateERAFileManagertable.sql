e9fe89eabea81b275ff51775e70adc976fb31e1c
Mohamed Khaled <muhammaed_khaled@yahoo.com>
2021-12-16 00:50:16 +0200

    Update CreateERAFileManagertable.sql

--- PracticeCompass.Data/Scripts/CreateERAFileManagertable.sql
+++ PracticeCompass.Data/Scripts/CreateERAFileManagertable.sql
@@ -2,12 +2,17 @@ USE [medman]
 GO
 
 /****** Object:  Table [dbo].[ERAFileManager]    Script Date: 12/10/2021 3:24:28 PM ******/
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[ERAFileManager]') AND type in (N'U'))
+DROP TABLE [dbo].[ERAFileManager]
+GO
+
 SET ANSI_NULLS ON
 GO
 
 SET QUOTED_IDENTIFIER ON
 GO
-CREATE TABLE [dbo].[ERAFileManager](
+
+CREATE  TABLE [dbo].[ERAFileManager](
 	[FileID] [int] IDENTITY(1,1) NOT NULL,
 	[FileName] [nvarchar](250) NOT NULL,
 	[Path] [nvarchar](250) NOT NULL,
