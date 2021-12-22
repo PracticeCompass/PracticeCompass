bef347011348a5c625e6d45b45f5b57b083e6169
Mohamed Khaled <muhammaed_khaled@yahoo.com>
2021-12-22 08:11:26 +0200

    Change Url Depeding on environment

--- PracticeCompass.ReactUI/ClientApp/src/config.js
+++ PracticeCompass.ReactUI/ClientApp/src/config.js
@@ -1,5 +1,4 @@
 const config = {
-  baseUrl: "http://localhost:51044/api",
-  //baseUrl: window.location.origin + "/PracticeCompassAPI/api",
+    baseUrl: process.env.NODE_ENV === "production" ? window.location.origin + "/PracticeCompassAPI/api" : "http://localhost:51044/api",
 };
 export default config;
